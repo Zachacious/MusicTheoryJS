1d1ea93725186b5beff217d01a1aeacd4e9fdae3
Zachacious <back4moore@gmail.com>
2020-01-19 11:54:25 -0600

    refactor chord interval

--- src/ChordInterval.js
+++ src/ChordInterval.js
@@ -5,9 +5,15 @@ Author: Zach Moore
 
 import { clampNumber } from './util';
 
-function ChordInterval(pInterval = 0/* 0 scale degree - root note */, pModifier = 1/* Natural */) {
-  this.interval = pInterval;
-  this.modifier = clampNumber(pModifier, 1, 5);
+class ChordInterval {
+  constructor(pInterval = 0/* 0 scale degree - root note */, pModifier = 1/* Natural */) {
+    this.interval = pInterval;
+    this.modifier = clampNumber(pModifier, 1, 5);
+  }
+
+  static create(pInterval = 0, pModifier = 1) {
+    return new ChordInterval(pInterval, pModifier);
+  }
 }
 
 export default ChordInterval;
