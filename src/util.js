d7c4f03961c95121e2ab8c539b315512976706f9
Zachacious <back4moore@gmail.com>
2020-01-19 13:49:10 -0600

    refactor wrapRange function

--- src/util.js
+++ src/util.js
@@ -9,33 +9,14 @@ const clampNumber = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.
 // const wrapRange = function (pValue, pLower, pUpper) {
 const wrapRange = (pValue, pLower, pUpper) => {
   let value = pValue;
-  let ltIter = 0; // less than iterations
-  let gtIter = 0; // greater than iterations
   let wraps = 0;
 
-  // If value < lower : add the upper
-  // until value higher than lower
-  while (value < pLower) {
-    ltIter += 1;
-    value += pUpper;
-    // eslint-disable-next-line no-console
-    console.log('lower');
-  }
-
-  // If value > upper : subtract the upper
-  // until value less than upper
-  while (value > pUpper) {
-    gtIter += 1;
-    value -= pUpper;
-  }
-
-  wraps -= clampNumber(ltIter, 0, 10);
-  wraps += clampNumber(gtIter, 0, 10);
+  wraps = Math.trunc(value / pUpper); // must truncate the remainder
+  value -= (pUpper * wraps);
 
   return { value, wraps };
 };
 
-// eslint-disable import/prefer-default-export
 export {
   wrapRange,
   clampNumber,
