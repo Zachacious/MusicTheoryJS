0acdb8898e6dc2966c3f447530c49e25afc011a2
zachacious <back4moore@gmail.com>
2021-11-25 13:33:28 -0600

    added documentation for tuning

--- src/Tuning.ts
+++ src/Tuning.ts
@@ -1,54 +1,78 @@
-// let _tuning: { [key: string]: any } = {
-//   a4: 440,
-// };
-
-// const tuning = (tuning?: { [key: string]: any }): { [key: string]: any } => {
-//   if (tuning) {
-//     _tuning = { ..._tuning, ...tuning };
-//   }
-
-//   return _tuning;
-// };
-
-// export default tuning;
 import { createTables } from "./Tables";
+
+//**********************************************************
+/**
+ * Tuning component used by Instrument class
+ * - containes the a4 tuning - default is 440Hz
+ * - builds lookup tables for midi key and frequency
+ * - based on the tuning
+ */
+//**********************************************************
 class Tuning {
-  private _a4: number = 440;
-  private _midiKeyTable: { [key: string]: number } = {};
-  private _freqTable: { [key: string]: number } = {};
+   /**
+    * Creates the object and builds the lookup tables.
+    */
+   constructor(a4Freq: number = 440) {
+      this._a4 = a4Freq;
+      this.buildTables();
+   }
 
-  private buildTables(): void {
-    const tables = createTables(this._a4);
-    this._midiKeyTable = tables.midiLookup;
-    this._freqTable = tables.freqLookup;
-  }
+   //**********************************************************
+   /**
+    * a4 Tuning
+    */
+   //**********************************************************
+   private _a4: number = 440;
 
-  /**
-   * Creates the object and builds the lookup tables.
-   */
-  constructor(a4Freq: number = 440) {
-    this._a4 = a4Freq;
-    this.buildTables();
-  }
+   public get a4(): number {
+      return this._a4;
+   }
 
-  public get a4(): number {
-    return this._a4;
-  }
+   //**********************************************************
+   /**
+    * setting the tuning will rebuild the lookup tables
+    */
+   //**********************************************************
+   public set a4(value: number) {
+      this._a4 = value;
+      this.buildTables();
+   }
 
-  public set a4(value: number) {
-    this._a4 = value;
-    this.buildTables();
-  }
+   //**********************************************************
+   /**
+    * lookup table for midi key
+    */
+   //**********************************************************
 
-  public midiKeyLookup(octave: number, semitone: number): number {
-    const key = `${octave}-${semitone}`;
-    return this._midiKeyTable[key];
-  }
+   private _midiKeyTable: { [key: string]: number } = {};
 
-  public freqLookup(octave: number, semitone: number): number {
-    const key = `${octave}-${semitone}`;
-    return this._freqTable[key];
-  }
+   public midiKeyLookup(octave: number, semitone: number): number {
+      const key = `${octave}-${semitone}`;
+      return this._midiKeyTable[key];
+   }
+
+   //**********************************************************
+   /**
+    * lookup table for frequency
+    */
+   //**********************************************************
+   private _freqTable: { [key: string]: number } = {};
+
+   public freqLookup(octave: number, semitone: number): number {
+      const key = `${octave}-${semitone}`;
+      return this._freqTable[key];
+   }
+
+   //**********************************************************
+   /**
+    * Builds the lookup tables for midi key and frequency
+    */
+   //**********************************************************
+   private buildTables(): void {
+      const tables = createTables(this._a4);
+      this._midiKeyTable = tables.midiLookup;
+      this._freqTable = tables.freqLookup;
+   }
 }
 
 export default Tuning;
