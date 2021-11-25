f4c24a68db628da22c59d9115adfe1d984163d21
zachacious <back4moore@gmail.com>
2021-11-25 13:39:03 -0600

    added documentation for instrument and tables

--- src/Instrument.ts
+++ src/Instrument.ts
@@ -2,21 +2,43 @@ import Tuning from "./Tuning";
 import Note from "./Note";
 import Identifiable from "./composables/Identifiable";
 
-@Identifiable()
+//**********************************************************
+/**
+ * Instrument class - used to represent an instrument
+ * used to encapsulate the tuning and retrieving midi keys
+ * and frequencies for notes
+ */
+//**********************************************************
+@Identifiable() // generates a unique id for each instance - use id() to get it
 class Instrument {
-  tuning: Tuning;
+   tuning: Tuning;
 
-  constructor(a4Freq: number = 440) {
-    this.tuning = new Tuning(a4Freq);
-  }
+   //**********************************************************
+   /**
+    * creates a new instance of an instrument with the given tuning or 440hz
+    */
+   //**********************************************************
+   constructor(a4Freq: number = 440) {
+      this.tuning = new Tuning(a4Freq);
+   }
 
-  getFrequency(note: Note): number {
-    return this.tuning.freqLookup(note.octave, note.semitone);
-  }
+   //**********************************************************
+   /**
+    * returns the frequency of the given note
+    */
+   //**********************************************************
+   getFrequency(note: Note): number {
+      return this.tuning.freqLookup(note.octave, note.semitone);
+   }
 
-  getMidiKey(note: Note): number {
-    return this.tuning.midiKeyLookup(note.octave, note.semitone);
-  }
+   //**********************************************************
+   /**
+    * returns the midi key of the given note
+    */
+   //**********************************************************
+   getMidiKey(note: Note): number {
+      return this.tuning.midiKeyLookup(note.octave, note.semitone);
+   }
 }
 
 export default Instrument;
--- src/Tables.ts
+++ src/Tables.ts
@@ -1,80 +1,76 @@
-import tuning from "./Tuning";
-
+//**********************************************************
+/**
+ * Constants
+ */
+//**********************************************************
 const MIDIKEY_START = 12;
 const NUM_OCTAVES = 10;
 const NUM_SEMITONES = 12;
 
-// /**
-//  * Maps octave and semitone to note frequency(hertz).
-//  * requires a key in the form of `<octave>-<semitone>`
-//  */
-// const freqLookup: { [key: string]: number } = {};
-
-// /**
-//  * Maps octave and semitone to midi key.
-//  * requires a key in the form of `<octave>-<semitone>`
-//  */
-// const midiLookup: { [key: string]: number } = {};
-
+//**********************************************************
 /**
  * Calculates the midi key for a given octave and semitone.
  */
-const calcMidiKey = (octave: number, semitone: number): number =>
-  MIDIKEY_START + octave * NUM_SEMITONES + semitone;
+//**********************************************************
+const calcMidiKey = (octave: number, semitone: number): number => MIDIKEY_START + octave * NUM_SEMITONES + semitone;
 
+//**********************************************************
 /**
  * Calculates the frequency for a given octave and semitone given
  * a tuning for a4.
  */
-const calcFrequency = (midiKey: number, a4Tuning: number): number =>
-  2 ** ((midiKey - 69) / 12) * a4Tuning;
+//**********************************************************
+const calcFrequency = (midiKey: number, a4Tuning: number): number => 2 ** ((midiKey - 69) / 12) * a4Tuning;
 
+//**********************************************************
 /**
  * Tables - return type for create tables
  */
+//**********************************************************
 type Tables = {
-  freqLookup: { [key: string]: number };
-  midiLookup: { [key: string]: number };
+   freqLookup: { [key: string]: number };
+   midiLookup: { [key: string]: number };
 };
 
+//**********************************************************
 /**
  * Creates and return lookup tables for midikey and frequency.
  */
+//**********************************************************
 const createTables: Function = (a4Tuning: number = 440): Tables => {
-  /**
-   * Maps octave and semitone to note frequency(hertz).
-   * requires a key in the form of `<octave>-<semitone>`
-   */
-  const freqTable: { [key: string]: number } = {};
+   //**********************************************************
+   /**
+    * Maps octave and semitone to note frequency(hertz).
+    * requires a key in the form of `<octave>-<semitone>`
+    */
+   //**********************************************************
+   const freqTable: { [key: string]: number } = {};
 
-  /**
-   * Maps octave and semitone to midi key.
-   * requires a key in the form of `<octave>-<semitone>`
-   */
-  const midiTable: { [key: string]: number } = {};
+   //**********************************************************
+   /**
+    * Maps octave and semitone to midi key.
+    * requires a key in the form of `<octave>-<semitone>`
+    */
+   //**********************************************************
+   const midiTable: { [key: string]: number } = {};
 
-  let iOctave: number = 0;
-  let iSemitone: number = 0;
+   let iOctave: number = 0;
+   let iSemitone: number = 0;
 
-  for (iOctave = 0; iOctave < NUM_OCTAVES; ++iOctave) {
-    for (iSemitone = 0; iSemitone < NUM_SEMITONES; ++iSemitone) {
-      const key = `${iOctave}-${iSemitone}`;
-      const mkey = calcMidiKey(iOctave, iSemitone);
-      const freq = calcFrequency(mkey, a4Tuning);
-      midiTable[key] = mkey;
-      freqTable[key] = freq;
-    }
-  }
+   for (iOctave = 0; iOctave < NUM_OCTAVES; ++iOctave) {
+      for (iSemitone = 0; iSemitone < NUM_SEMITONES; ++iSemitone) {
+         const key = `${iOctave}-${iSemitone}`;
+         const mkey = calcMidiKey(iOctave, iSemitone);
+         const freq = calcFrequency(mkey, a4Tuning);
+         midiTable[key] = mkey;
+         freqTable[key] = freq;
+      }
+   }
 
-  return {
-    freqLookup: freqTable,
-    midiLookup: midiTable,
-  };
+   return {
+      freqLookup: freqTable,
+      midiLookup: midiTable,
+   };
 };
-
-// /**
-//  * Will create the lookup tables the first time this file is loaded.
-//  */
-// createTables();
 
 export { createTables };
