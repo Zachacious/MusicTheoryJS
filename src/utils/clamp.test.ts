e3147db1b81108ae9c23508979d59a5cf9014f1d
zachacious <back4moore@gmail.com>
2021-11-24 23:08:42 -0600

    added clamp test

---
+++ src/utils/clamp.test.ts
@@ -0,0 +1,16 @@
+import clamp from "./clamp";
+
+describe("Clamp Util", () => {
+  it("should clamp a number between a min and max", () => {
+    expect(clamp(10, 0, 10)).toBe(10);
+    expect(clamp(10, 10, 0)).toBe(10);
+    expect(clamp(10, 0, 0)).toBe(0);
+    expect(clamp(10, 10, 10)).toBe(10);
+    expect(clamp(10, 5, 15)).toBe(10);
+    expect(clamp(10, 15, 5)).toBe(10);
+    expect(clamp(10, -5, 15)).toBe(10);
+    expect(clamp(10, 15, -5)).toBe(10);
+    expect(clamp(10, -5, -15)).toBe(-5);
+    expect(clamp(10, -15, -5)).toBe(-5);
+  });
+});
